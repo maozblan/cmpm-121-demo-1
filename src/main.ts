7aab7bb12bad63dda527201691c9c3032a858c86
Lyssa Li <lyssa.li137@gmail.com>
2024-10-07 07:24:13 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,17 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// clicker button
 const clicker: HTMLButtonElement = document.createElement("button");
 clicker.textContent = "☕"; // coffee emoji (it's just small)
+clicker.onclick = () => {
+  coffeeCount++;
+  counter.textContent = `${coffeeCount} coffees`;
+};
 app.append(clicker);
+
+// coffee counter
+const counter: HTMLDivElement = document.createElement("div");
+let coffeeCount: number = 0;
+counter.textContent = `${coffeeCount} coffees`;
+app.append(counter);
