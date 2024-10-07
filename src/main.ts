2286386e7e27583e490c46406d3bb463ece9ee9b
Lyssa Li <lyssa.li137@gmail.com>
2024-10-07 07:18:25 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "boopadoope";
+const gameName = "boopadoop";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const clicker: HTMLButtonElement = document.createElement("button");
+clicker.textContent = "☕"; // coffee emoji (it's just small)
+app.append(clicker);
