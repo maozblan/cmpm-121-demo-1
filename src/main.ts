54a9d30084ae38f2e4ac48efea6a51c82fba23d2
Lyssa Li <lyssa.li137@gmail.com>
2024-10-11 11:53:52 -0700

    attempted to overcomplicate the graphics

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import "./style.css";
 import { sidebar } from "./allTabs";
+import { graphics } from "./graphics";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "boopadoop";
 document.title = gameName;
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+// const header = document.createElement("h1");
+// header.innerHTML = gameName;
+// app.append(header);
 
-const graphics: HTMLDivElement = document.createElement("div");
+// append graphics
 app.append(graphics);
+graphics.id = "graphics";
 
+// append sidebar
 app.append(sidebar.sidebar);
+sidebar.sidebar.id = "sidebar";
+
+// open sidebar on load
 document.getElementById("garden")?.click();
